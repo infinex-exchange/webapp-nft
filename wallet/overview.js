9aea43959d114b329856a02dff12e04465e86002
vayamos-exchange <admin@vayamos.cc>
2022-11-02 23:22:00 +0100

    Card and image h100

--- wallet/overview.js
+++ wallet/overview.js
@@ -38,8 +38,8 @@ $(document).on('authChecked', function() {
                         $.each(data.nfts, function(nftid, nft) {
                             thisAS.append(`
                                 <div class="nft-item col-12 col-md-3 col-lg-3 py-2">
-                                    <div class="card hoverable">
-										<img src="${nft.preview}" class="card-img-top">
+                                    <div class="card h-100 hoverable">
+										<img src="${nft.preview}" class="card-img-top h-100">
 										<div class="card-body">
 											<h5 class="card-title">${nft.name}</h5>
 											<span class="small secondary">
