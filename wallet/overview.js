87560d84cad31f1dc25dea555f35d24f2a0bd110
vayamos-exchange <admin@vayamos.cc>
2022-11-02 23:16:09 +0100

    Padding

--- wallet/overview.js
+++ wallet/overview.js
@@ -37,7 +37,7 @@ $(document).on('authChecked', function() {
                     if(data.success) {
                         $.each(data.nfts, function(nftid, nft) {
                             thisAS.append(`
-                                <div class="nft-item col-12 col-md-3 col-lg-3">
+                                <div class="nft-item col-12 col-md-3 col-lg-3 py-2">
                                     <div class="card hoverable">
 										<img src="${nft.preview}" class="card-img-top">
 										<div class="card-body">
