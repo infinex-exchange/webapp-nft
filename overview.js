0ed09c20c4c6377f8a5080031da28cd435f0b4de
vayamos-exchange <admin@vayamos.cc>
2022-11-03 11:46:07 +0100

    Htaccess test

---
+++ overview.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    window.renderingStagesTarget = 1;
+    
+    $('#nft-search').on('input', function() {
+        var query = $(this).val();
+        if(query == '')
+            delete window.nftsAS.data.search;
+        else
+            window.nftsAS.data.search = query;
+        window.nftsAS.reset();
+    });
+});
+
+$(document).on('authChecked', function() {
+    if(window.loggedIn) {
+        window.nftsAS = new AjaxScroll(
+            $('#nfts-data'),
+            $('#nfts-data-preloader'),
+            {
+                api_key: window.apiKey
+            },
+            function() {
+                
+                //---
+                this.data.offset = this.offset;
+                var thisAS = this;
+                
+                $.ajax({
+                    url: config.apiUrl + '/nft/wallet/nfts',
+                    type: 'POST',
+                    data: JSON.stringify(thisAS.data),
+                    contentType: "application/json",
+                    dataType: "json",
+                })
+                .retry(config.retry)
+                .done(function (data) {
+                    if(data.success) {
+                        $.each(data.nfts, function(nftid, nft) {
+                            thisAS.append(`
+                                <div class="nft-item col-12 col-md-3 col-lg-3 py-2">
+                                    <div class="card h-100 hoverable">
+										<img src="${nft.preview}" class="card-img-top h-100">
+										<div class="card-body">
+											<h5 class="card-title">${nft.name}</h5>
+											<span class="small secondary">
+											    <img src="${data.networks[nft.network].icon_url}" width="16" height="16">
+											    ${data.networks[nft.network].description}
+											</span>
+									    </div>
+									</div>
+                                </div>
+                            `);
+                        });
+                        
+                        thisAS.done();
+                
+                        if(thisAS.offset == 0)
+                            $(document).trigger('renderingStage');
+                            
+                        if(data.nfts.length != 50)
+                            thisAS.noMoreData();
+                    } else {
+                        msgBoxRedirect(data.error);
+                        thisAS.done();
+                        thisAS.noMoreData();
+                    }
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    msgBoxNoConn(true);
+                    thisAS.done();
+                    thisAS.noMoreData();
+                });
+                //---
+                
+            },
+            true,
+            true
+        );
+    }
+});
---
+++ overview.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('../../../templates/head.php'); ?>
+        <script src="/js/ajax_scroll.js?<?php echo filemtime(__DIR__.'/../../../js/ajax_scroll.js'); ?>"></script>
+        <link rel="stylesheet" href="/nft/css/styles.css?<?php echo filemtime(__DIR__.'/../css/styles.css'); ?>">
+        <title>NFT wallet | Vayamos NFT</title>
+    </head>
+    <body>
+    
+        <!-- Preloader -->
+        <?php include('../../../templates/preloader.html'); ?>
+        
+        <!-- Navbar -->
+        <?php include('../../../templates/navbar.php'); ?>
+        
+        <!-- Root container -->
+        <div id="root" class="container-fluid container-1500 p-0 user-only">
+        <div class="row m-0 h-rest">
+        
+        <!-- Main column -->
+        <div class="col-12 p-0 ui-card ui-column">
+            
+            <form>
+            <div class="row">
+                <div class="col-auto order-1 my-auto p-1 p-lg-2">
+                    <a href="/nft/wallet/deposit" class="btn btn-primary btn-sm">Deposit</a>
+                    <a href="/nft/wallet/withdraw" class="btn btn-primary btn-sm">Withdraw</a>
+                    <a href="/nft/wallet/transfer" class="btn btn-primary btn-sm">Transfer</a>
+                </div>
+                
+                <div class="col-12 col-lg-auto order-3 order-lg-2 my-auto p-1 p-lg-2">
+                    <input id="nft-search" type="text" size="7" placeholder="Search" class="form-control input-search">
+                </div>
+                
+                <!--
+                <div class="col-auto order-2 order-lg-3 ms-auto ms-lg-0 my-auto p-1 p-lg-2">
+                    <a href="/nft/create" class="btn btn-primary btn-sm">Create NFT</a>
+                </div>
+                -->
+            </div>
+            </form>
+            
+            <div class="row" id="nfts-data">
+            </div>
+        
+        <!-- / Main column -->
+        </div>
+            
+        <!-- / Root container -->    
+        </div>
+        </div>
+        
+        <script src="/nft/wallet/overview.js?<?php echo filemtime(__DIR__.'/overview.js'); ?>"></script>
+        
+        <?php include('../../../templates/modals.php'); ?>
+        <?php include('../templates/mobile_nav.php'); ?>
+    
+    </body>
+</html>
