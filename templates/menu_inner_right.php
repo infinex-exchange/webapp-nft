414b5eb38b7eab6c1d2b621df19f54adb1505d09
vayamos-exchange <admin@vayamos.cc>
2022-11-03 16:37:39 +0100

    Add dropdown class

--- templates/menu_inner_right.php
+++ templates/menu_inner_right.php
@@ -1,4 +1,4 @@
-<li class="nav-item user-only">
+<li class="nav-item user-only dropdown">
     <a href="#_" class="nav-link dropdown-toggle auto-active-group" data-bs-toggle="dropdown" aria-expanded="false">
         <i class="fa-solid fa-wallet"></i><br>
         NFT Wallet
