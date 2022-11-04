065fee78587b36c61a40194d0eaf675d3fc3242e
vayamos-exchange <admin@vayamos.cc>
2022-11-04 14:17:06 +0100

    Details skel

---
+++ details.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('../../templates/head.php'); ?>
+        <link rel="stylesheet" href="/nft/css/styles.css?<?php echo filemtime(__DIR__.'/css/styles.css'); ?>">
+        <title>Vayamos NFT</title>
+    </head>
+    <body>
+    
+        <!-- Preloader -->
+        <?php include('../../templates/preloader.html'); ?>
+        
+        <!-- Navbar -->
+        <?php include('../../templates/navbar.php'); ?>
+        <?php include(__DIR__.'/templates/navbar.php'); ?>
+        
+        <!-- Root container -->
+        <div id="root" class="container-fluid container-1500 container-rest p-0">
+        <div class="row m-0 h-rest">
+        
+        <!-- Main column -->
+        <div class="col-12 p-0 ui-card ui-column">
+            
+            <div class="row p-2">
+                <h3 class="nft-name"></h3>
+            </div>
+        
+        <!-- / Main column -->
+        </div>
+            
+        <!-- / Root container -->    
+        </div>
+        </div>
+        
+        <?php include('../../templates/modals.php'); ?>
+        <script src="/nft/js/details.js?<?php echo filemtime(__DIR__.'/js/details.js'); ?>"></script>
+        
+        <?php include(__DIR__.'/templates/mobile_nav.php'); ?>
+    
+    </body>
+</html>
---
+++ js/details.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    window.renderingStagesTarget = 1;
+    
+    var pathArray = window.location.pathname.split('/');
+    var nftid = pathArray[pathArray.length - 1];
+    
+    $.ajax({
+        url: config.apiUrl + '/nft/details',
+        type: 'POST',
+        data: JSON.stringify({
+            nftid: nftid
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            $('.nft-name').html(data.nft.name);
+            
+            $(document).trigger('renderingStage');
+        }
+        
+        else {
+            msgBoxRedirect(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true);
+    });
+});
