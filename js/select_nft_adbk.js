e5101c359881fa4605133fbffbad3465de73a666
vayamos-exchange <admin@vayamos.cc>
2022-11-05 10:07:59 +0100

    Nft addressbook

---
+++ js/select_nft_adbk.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $('#select-nft-adbk').on('click', function(event) {
+        $('.selector-dropdown').not('#select-nft-adbk-dropdown').hide();
+        $('.selector-arrow').not('#select-nft-adbk-arrow').removeClass('flip');
+        
+        $('#select-nft-adbk-dropdown').toggle();
+        $('#select-nft-adbk-arrow').toggleClass('flip');
+        
+        event.stopPropagation();
+    });
+    
+    $('#select-nft-adbk').on('input', function() {
+        $('#select-nft-adbk-dropdown').hide();
+        $('#select-nft-adbk-arrow').removeClass('flip');
+    });
+    
+    $('html').on('click', function() {
+        $('#select-nft-adbk-dropdown').hide();
+        $('#select-nft-adbk-arrow').removeClass('flip');
+    });
+});
+
+function initSelectNftAdbk(network) {
+    $('#select-nft-adbk-data').empty();
+    
+    $.ajax({
+        url: config.apiUrl + '/nft/wallet/addressbook',
+        type: 'POST',
+        data: JSON.stringify({
+            api_key: window.apiKey,
+            network: network
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            $.each(data.addressbook, function(k, v) {
+                $('#select-adbk-data').append(`
+                    <div class="select-nft-adbk-item row p-1 hoverable" data-address="${v.address}">
+                        <div class="col-12">
+                            <h5 class="secondary">${v.name}</h5>
+                            ${v.address}
+                        </div>
+                    </div>
+                `);
+            });
+                
+            $('.select-nft-adbk-item').on('click', function() {
+                $('#select-nft-adbk').val($(this).data('address')).trigger('input');
+            });
+        } else {
+            msgBox(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true);
+    });
+}
---
+++ templates/select_nft_adbk.php
@@ -0,0 +1,10 @@
+<div class="selector-wrapper">
+    <div class="selector-inner">
+        <input id="select-nft-adbk" type="text" placeholder="Paste address or select from address book" class="form-control selector-input" autocomplete="off">
+        <i id="select-nft-adbk-arrow" class="fa-solid fa-angle-down flippable selector-arrow"></i>
+    </div>
+    <div id="select-nft-adbk-dropdown" class="selector-dropdown">
+        <div id="select-nft-adbk-data" class="scrollable selector-data"></div>
+    </div>
+</div>
+<script src="/nft/js/select_nft_adbk.js?<?php echo filemtime(__DIR__.'/../js/select_nft_adbk.js'); ?>"></script>
