8524b3dcf89ff8ec48fbe7e465c9c7a640da0ced
vayamos-exchange <admin@vayamos.cc>
2022-11-10 16:32:45 +0100

    Single offer view price refactor part3

--- offer.php
+++ offer.php
@@ -62,7 +62,7 @@
                                 <h5 class="secondary">Final price:</h5>
                                 <h3 id="price-final"></h3>
                             </div>
-                            <div id="final-nobids" class="col-6 col-lg-3">
+                            <div id="final-nobids" class="col-12">
                                 <h5 class="secondary">Item has not been sold</h5>
                             </div>
                         </div>
