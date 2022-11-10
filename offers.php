73653c10b36e90e07887719b440c5e73129e0bec
vayamos-exchange <admin@vayamos.cc>
2022-11-10 11:57:30 +0100

    pull down test

--- offers.php
+++ offers.php
@@ -84,14 +84,11 @@
             <div class="col-12">
                 <?php include(__DIR__.'/../../templates/select_coin.php'); ?>
             </div>
-            
-            <div class="col-12 h-100">
-                <div class="ms-auto">
-                    test
-                </div>
-            </div>
         
         <!-- / Left column -->
+        </div>
+        <div class="row ms-auto">
+        test
         </div>
         </div>
         
