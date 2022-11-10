79f27b8ca214e1d42c0bb82fc709a21848bb65ea
vayamos-exchange <admin@vayamos.cc>
2022-11-10 17:11:49 +0100

    Desktop menu account

---
+++ templates/menu_inner_center.php
@@ -0,0 +1,11 @@
+<li class="nav-item user-only dropdown">
+    <a href="#_" class="nav-link dropdown-toggle auto-active-group" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fa-solid fa-wallet"></i><br>
+        NFT Wallet
+    </a>
+    <div class="dropdown-menu dropdown-menu-end">
+        <ul class="nav flex-column">
+            <?php include('menu_nft_wallet.html'); ?>
+        </ul>
+    </div>
+</li>
