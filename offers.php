0f32efa1aebe2eedbaf2be651afe078635a8439b
vayamos-exchange <admin@vayamos.cc>
2022-11-10 10:13:19 +0100

    Ui cards always visible on desktop

--- offers.php
+++ offers.php
@@ -22,7 +22,7 @@
         <div class="row m-0">
         
         <!-- Left column -->
-        <div class="col-12 col-lg-3 p-0 ui-card ui-column rest-of-height" data-ui-card="filter">
+        <div class="col-12 col-lg-3 p-0 ui-card ui-column rest-of-height d-lg-block" data-ui-card="filter">
         <div class="row">
         
             <div class="col pb-4">
@@ -77,7 +77,7 @@
         </div>
         
         <!-- Main column -->
-        <div id="offers" class="col-12 col-lg-9 p-0 ui-card ui-column rest-of-height" data-ui-card="offers">
+        <div id="offers" class="col-12 col-lg-9 p-0 ui-card ui-column rest-of-height d-lg-block" data-ui-card="offers">
             <div id="offers-header" class="row pb-2">
                 <div class="col-auto my-auto d-none d-lg-block">
 	                <h3>Offers list</h3>
