a7ccb9d660527ebaab6544e1f76bdb0f9a3c6e1c
vayamos-exchange <admin@vayamos.cc>
2022-11-11 12:53:10 +0100

    Layout

--- js/render_offer.js
+++ js/render_offer.js
@@ -161,7 +161,7 @@ function renderOfferHor(offer) {
     var endTime = new Date(offer.end_time * 1000).toLocaleString();
                                 
     return `
-        <div class="col-12 my-offer-item row hoverable px-2 py-3" data-noid="${offer.noid}">
+        <div class="col-12 my-offer-item hoverable px-2 py-3" data-noid="${offer.noid}">
         <div class="row">
             <div class="col-2">
                 <a href="/nft/offer/${offer.noid}">
