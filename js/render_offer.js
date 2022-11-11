ac6ef46280cdb6e916cebe8d58e8d4f5907ed394
vayamos-exchange <admin@vayamos.cc>
2022-11-11 14:48:18 +0100

    Manage button

--- js/render_offer.js
+++ js/render_offer.js
@@ -168,6 +168,9 @@ function renderOfferHor(offer, manage = false) {
     if(manage) {
         manageHtml = `
             <div class="col-12 pt-3">
+                <strong class="secondary">Item has not been sold</strong>
+            </div>
+            <div class="col-12 pt-3">
                 <a href="/nft/sell/${offer.nftid}" class="btn btn-primary">
                     <i class="fa-solid fa-arrow-rotate-right"></i>
                     Renew listing
