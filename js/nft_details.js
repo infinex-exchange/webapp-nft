1956df175f0e40e165c44f4a380a46ab15b10854
infinex-exchange <admin@vayamos.cc>
2023-02-19 09:39:53 +0100

    Fix details of nft without collection

--- js/nft_details.js
+++ js/nft_details.js
@@ -50,7 +50,7 @@ function getNftDetails(nftid, callback = null) {
             });
             $('#nft-attributes').html(attributesHtml == '' ? '-' : `<ul class="m-0">${attributesHtml}</ul>`);
             
-            if(data.nft.collection) !== null) {
+            if(data.nft.collection !== null) {
                 var colNameHtml = data.nft.collection.name;
                 
                 if(data.nft.collection.icon_url !== null)
